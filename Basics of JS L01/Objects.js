b13b8b92626b4c0615295a74fe7879755d32493e
unnati-cmd <unnatiald@gmail.com>
2026-02-16 13:40:12 +0530

    Object basics

--- Basics of JS L01/Objects.js
+++ Basics of JS L01/Objects.js
@@ -14,3 +14,20 @@ for(let key in obj) {
     console.log(key, obj[key]);
 }
 
+//Destructure the key "first-name" as a variable called firsName.
+const user = {
+    "first-name": "Unnati",
+};
+//when we have such key names, we have to specify another name which can be used to refer to this value.
+let {"first-name" : firstName} = user;
+
+//use object.entries() to put all the key-value pairs as:
+// title: JavaScript
+// duration: 4 weeks
+const course = {
+    title: "JavaScrip",
+    duration: "4 weeks",
+};
+Object.entries(course).forEach(function(val){
+    console.log(val[0] + ": " + val[1]);
+})

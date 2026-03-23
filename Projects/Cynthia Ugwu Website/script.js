1d11c807c510b31be75eb934b046cdd5178cd88b
unnati-cmd <unnatiald@gmail.com>
2026-03-23 14:53:50 +0530

    Scroll pointer changed

--- Projects/Cynthia Ugwu Website/script.js
+++ Projects/Cynthia Ugwu Website/script.js
@@ -5,9 +5,26 @@ const scroll = new LocomotiveScroll({
 
 function circleMouseFollower(){
     window.addEventListener('mousemove',function(dets){
-        console.log(dets);
-        document.querySelector('#minicircle').style.transform = `translate(${dets.clientX - 3}px, ${dets.clientY - 3}px)`
+        document.querySelector('#minicircle').style.transform = `translate(${dets.clientX - 4}px, ${dets.clientY - 4}px)`
     });
 }
 
 circleMouseFollower();
+
+const second = document.querySelectorAll('.elem');
+const cursor = document.querySelector('#minicircle');
+
+second.forEach((el) => {
+  el.addEventListener("mouseenter", () => {
+    cursor.classList.add("active");
+    cursor.innerHTML = "VIEW";
+    cursor.style.display = 'flex';
+    cursor.style.alignItems = 'center';
+    cursor.style.justifyContent = 'center';
+  });
+  el.addEventListener("mouseleave", () => {
+    cursor.classList.remove("active");
+    cursor.innerHTML = "";
+  });
+ 
+});

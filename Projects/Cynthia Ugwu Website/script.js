ea1dae0698b64612ef819a69ae52510302f9932e
unnati-cmd <unnatiald@gmail.com>
2026-03-23 21:09:09 +0530

    Locomotive scroll and cutom cursor issue resolved

--- Projects/Cynthia Ugwu Website/script.js
+++ Projects/Cynthia Ugwu Website/script.js
@@ -1,26 +1,20 @@
+const second = document.querySelectorAll('.elem');
+const cursor = document.querySelector('#minicircle');
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
 
-function circleMouseFollower(){
-    window.addEventListener('mousemove',function(dets){
-        document.querySelector('#minicircle').style.transform = `translate(${dets.clientX - 4}px, ${dets.clientY - 4}px)`
-    });
-}
-
-circleMouseFollower();
-
-const second = document.querySelectorAll('.elem');
-const cursor = document.querySelector('#minicircle');
+window.addEventListener('mousemove', function(e){
+    const scale = cursor.classList.contains("active") ? 6 : 1;
+    cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px) scale(${scale})`;
+});
 
 second.forEach((el) => {
   el.addEventListener("mouseenter", () => {
     cursor.classList.add("active");
     cursor.innerHTML = "VIEW";
-    cursor.style.display = 'flex';
-    cursor.style.alignItems = 'center';
-    cursor.style.justifyContent = 'center';
   });
   el.addEventListener("mouseleave", () => {
     cursor.classList.remove("active");

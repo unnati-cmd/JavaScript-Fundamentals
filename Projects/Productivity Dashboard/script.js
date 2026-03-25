9bbe9c9ff33861647aa8234111b16dfba93d808e
unnati-cmd <unnatiald@gmail.com>
2026-03-26 03:08:39 +0530

    Front UI Basics completed

---
+++ Projects/Productivity Dashboard/script.js
@@ -0,0 +1,15 @@
+var allElems = document.querySelectorAll('.elem');
+var FullElemsPage = document.querySelectorAll('.fullElements')
+var allFullElemsBackBtn = document.querySelectorAll('.fullElements .back')
+
+allElems.forEach(function(elem){
+    elem.addEventListener('click', function(){
+        FullElemsPage[elem.id].style.display = 'block';
+    })
+})
+
+allFullElemsBackBtn.forEach(function(back){
+    back.addEventListener('click',function(){
+        FullElemsPage[back.id].style.display = 'none';
+    })
+})

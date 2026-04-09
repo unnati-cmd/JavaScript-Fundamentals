84b9909fff44f984596b196c488d00821e54b64a
unnati-cmd <unnatiald@gmail.com>
2026-04-09 23:33:45 +0530

    ToDo list completed.

--- Projects/Productivity Dashboard/script.js
+++ Projects/Productivity Dashboard/script.js
@@ -17,20 +17,64 @@ function openFeatures(){
 }
 openFeatures();
 
+
+
 // To Do List JS
 let form = document.querySelector('.addTask form');
+
+var currentTask = []
+if(localStorage.getItem('currentTask')) {
+    currentTask = JSON.parse(localStorage.getItem('currentTask'));
+}else {
+    console.log('Task list is Empty.');
+}
+
+function renderTask(){
+    var allTask = document.querySelector('.allTask')
+    var sum = '';
+    currentTask.forEach(function(elem,idx){
+        sum = sum + `<div class='task'>
+            <details>
+                <summary>
+                    <h5>${elem.task}<span class=${elem.imp}>Imp</span></h5>
+                </summary>
+                <p>${elem.details}</p>
+            </details>
+            <button id=${idx}>Mark as Completed</button>
+        </div>`
+    })
+    allTask.innerHTML = sum;
+    localStorage.setItem('currentTask', JSON.stringify(currentTask)) ;
+}
+renderTask();
+
+
 let taskInput = document.querySelector('.addTask form #task-input');
 let taskDetailsInput = document.querySelector('.addTask form textarea');
 let taskCheckBox = document.querySelector('.addTask form #check')
 
-let allTask = [
-    {
-        task:'Mandir Jao',
-        details: 'Hanuman ji wale',
-        imp: true
-    },
-]
-
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    if(taskInput.value == '' || taskDetailsInput.value == ''){
+        alert('Please enter a task and description');
+        return;
+    }
+    currentTask.push(
+    {
+        task: taskInput.value, 
+        details: taskDetailsInput.value, 
+        imp: taskCheckBox.checked
+    })
+    renderTask();
+    location.reload();
+})
+
+//Mark as completed button 
+var markCompletedBtn = document.querySelectorAll('.task button');
+markCompletedBtn.forEach(function(btn){
+    btn.addEventListener('click',function(){
+        currentTask.splice(btn.id, 1);
+        renderTask();
+        location.reload();
+    })
 })
